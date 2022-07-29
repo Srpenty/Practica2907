b50f5f098cf0847d758a7fe4d5f2861aaedb76f8
Oscar <Oscar@LAPTOP-2H49CU0U>
2022-07-29 09:57:17 -0600

    Segundo programa

---
+++ App2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variables
+            string firstname = "Oscar";
+            String lastname = "Alvarez";
+            int edad = 19;
+            //Concatenacion de variables
+            var name = firstname + " " + lastname;
+            //Imprimir variables dentro del texto
+            Console.WriteLine("Su nombre es: {0} y su edad es: {1}", name, edad);
+        }
+    }
+}
--- Practica2907/Program.cs
+++ Practica2907/Program.cs
@@ -6,7 +6,16 @@ namespace Practica2907
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaracion de variables 
+            int num1 = 5;
+            int num2 = 4;
+            var total = num1 + num2;
+
+            string name = "Oscar";
+            int edad = 19;
+            //Impresion de datos
+            Console.WriteLine("La suma es:" + total);
+            Console.WriteLine(name + " " + edad);
         }
     }
 }
