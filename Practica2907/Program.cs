229c330f546111398812d99715afbe98499053da
Oscar <Oscar@LAPTOP-2H49CU0U>
2022-07-29 08:59:18 -0600

    Agregar archivos de proyecto.

---
+++ Practica2907/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica2907
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
