0d3336ddb1434122c9c316a6846aecfc29c680e9
Oscar <Oscar@LAPTOP-2H49CU0U>
2022-07-29 10:25:20 -0600

    Programa 5

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdusca su nombre: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Introdusca su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Su nombre es: {0} y tiene {1} años de edad", name, edad);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdusca un numero: ");
+            int num1 =int.Parse(Console.ReadLine());
+            Console.WriteLine("Introdusca otro numero: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("la suma de los numeros es: "+ (num1 + num2));
+            Console.WriteLine("la resta de los numeros es: " + (num1 - num2));
+            Console.WriteLine("la multiplicacion de los numeros es: " + (num1 * num2));
+            Console.WriteLine("la divicion de los numeros es: " + (num1 / num2));
+        }
+    }
+}
