0d3b53ca32663f97657ecb9b561d76d10ee34ac4
Oscar <Oscar@LAPTOP-2H49CU0U>
2022-07-30 21:06:26 -0600

    Tarea

---
+++ Tarea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer numero")
+            int num1 = Console.ReadLine;
+            Console.WriteLine("Ingrese el segundo numero")
+            int num2 = Console.ReadLine;
+            var total = num1 + num2;
+            Console.WriteLine("El valor de la suma es de: "+ total)
+        }
+    }
+}
